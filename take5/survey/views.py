ec5baf1e1f09fed969a7cd9801a65f68f34e36e1
daniel-root <albuquerquedaniel898@gmail.com>
2021-04-15 17:09:14 -0300

    criação de views

--- take5/survey/views.py
+++ take5/survey/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
+from .serializers import SurveySerializer, SurveyQuestionSerializer, SurveyQuestionAlternativeSerializer, SurveyUserAnswerSerializer
+from .models import Survey, SurveyQuestion, SurveyQuestionAlternative, SurveyUserAnswer
 # Create your views here.
+class SurveyViewSet(viewsets.ModelViewSet):
+    queryset = Survey.objects.all()
+    serializer_class = SurveySerializer
