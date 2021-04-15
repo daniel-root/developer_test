9ea77f7bfe8b12086ba2385228027cdc71f2ec23
daniel-root <albuquerquedaniel898@gmail.com>
2021-04-15 15:21:47 -0300

    modelos da aplicação

--- take5/survey/models.py
+++ take5/survey/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Survey(models.Model):
+    pass
+
+class SurveyQuestion(models.Model):
+    pass
+
+class SurveyQuestionAlternative(models.Model):
+    pass
+
+class SurveyUserAnswer(models.Model):
+    pass
