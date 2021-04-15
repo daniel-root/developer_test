ec066c82512085036b2db997c2b00a64503b4e6c
daniel-root <albuquerquedaniel898@gmail.com>
2021-04-15 15:33:38 -0300

    criando pesquisa utilizando o Painel do Django

--- take5/survey/admin.py
+++ take5/survey/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import Survey, SurveyQuestion, SurveyQuestionAlternative, SurveyUserAnswer
 
 # Register your models here.
+
+
+admin.site.register(Survey)
+admin.site.register(SurveyQuestion)
+admin.site.register(SurveyQuestionAlternative)
+admin.site.register(SurveyUserAnswer)
