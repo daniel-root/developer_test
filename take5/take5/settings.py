853ffba2ab0e5004be469224e2c3ceb669db7f5a
daniel-root <albuquerquedaniel898@gmail.com>
2021-04-17 09:52:30 -0300

    ajuste local e add db

--- take5/take5/settings.py
+++ take5/take5/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'take5.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_take5',
+	'USER': 'db_root',
+	'PASSWORD': 'HInduCcirLcDBAb6',
+	'HOST': 'localhost',
+	'PORT': '3306',
     }
 }
 
@@ -105,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
